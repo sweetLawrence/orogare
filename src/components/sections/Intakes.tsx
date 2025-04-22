188cd34732b554aac79d0a30fdaaf522985fed10
Lawrence <tsungulawrence@gmail.com>
2025-04-22 15:20:09 +0300

    code

--- src/components/sections/Intakes.tsx
+++ src/components/sections/Intakes.tsx
@@ -5,17 +5,20 @@ const Intakes = () => {
     {
       title: 'January Intake',
       from: '1st Jan 2025',
-      to: '1st Jan 2025'
+      to: '3rd Feb 2025',
+      status:'Closed'
     },
     {
       title: 'May Intake',
-      from: '1st Jan 2025',
-      to: '1st Jan 2025'
+      from: '20th Apr 2025',
+      to: '1st June 2025',
+      status:'Open'
     },
     {
       title: 'September Intake',
-      from: '1st Jan 2025',
-      to: '1st Jan 2025'
+      from: '1st Aug 2025',
+      to: '3rd Oct 2025',
+      status:'Closed'
     }
   ]
   return (
@@ -31,16 +34,17 @@ const Intakes = () => {
 
       <Text className='text-text-500'>
         We offer three major intakes each year, providing multiple opportunities
-        for students to join our programs. Whether you are looking to start in 
-        <b> January, May,</b>  or <b>September,</b> our flexible admission schedule ensures you
-        can begin your academic journey at a convenient time.
+        for students to join our programs. Whether you are looking to start in
+        <b> January, May,</b> or <b>September,</b> our flexible admission
+        schedule ensures you can begin your academic journey at a convenient
+        time.
       </Text>
 
       <div className='flex flex-col md:flex-row justify-between mt-5'>
         {opening.map((item, i) => (
           <Card
             key={i}
-            className='flex flex-row bg-gradient-to-b from-secondary-400 to-primary-500 md:w-[32%] mb-5 md:mb-0 h-[300px] relative rounded-lg'
+            className={`flex flex-row bg-gradient-to-b ${item.status === 'Open'?'from-secondary-400 to-primary-500':'bg-primary-950'}  md:w-[32%] mb-5 md:mb-0 h-[300px] relative rounded-lg`}
           >
             <Card.Section className='bg-white absolute right-0 !h-[100%] w-[93%] px-3 py-7'>
               <Title className='text-[1.5em] text-primary-700'>
@@ -55,9 +59,12 @@ const Intakes = () => {
                 {item.to}
               </Text>
             </Card.Section>
-            <Button className='absolute bg-gradient-to-r from-secondary-400 to-primary-500 bottom-[10%] left-[15%] w-[27%]'>
-              Apply
-            </Button>
+            <div className='g'>
+              <Button className='absolute bg-gradient-to-r from-secondary-400 to-primary-500 bottom-[10%] left-[15%] w-[27%]'>
+                Apply
+              </Button>
+              <div className={`absolute bg-gradient-to-r bottom-[12%] right-[15%] w-[27%] text-[1.2em] font-bold ${item.status=='Closed'? 'text-red-500':'text-green-500'}`}>{item.status}</div>
+            </div>
           </Card>
         ))}
       </div>
