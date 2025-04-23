5a62576df021a2667d6d1c734f064c4f1381a99c
Lawrence <tsungulawrence@gmail.com>
2025-04-23 12:13:18 +0300

    courses

--- src/data/coursesData.ts
+++ src/data/coursesData.ts
@@ -10,6 +10,9 @@ export type CourseCategory = {
   courses: Course[]
 }
 
+const cdacc_exam_body:string = 'TVET-CDACC'
+const ssc_exam_body:string = 'NITA'
+
 const diploma_duration: number = 3
 const craft_duration: number = 2
 const artisan_duration: number = 2
@@ -21,31 +24,31 @@ export const coursesData: CourseCategory[] = [
       {
         name: 'Diploma in Civil Engineering',
         minimumGrade: 'C-',
-        examiningBody: 'KNEC',
+        examiningBody: cdacc_exam_body,
         duration: `${diploma_duration} years`
       },
       {
         name: 'Diploma in Information and Communications Technology',
         minimumGrade: 'C-',
-        examiningBody: 'KNEC',
+        examiningBody: cdacc_exam_body,
         duration: `${diploma_duration} years`
       },
       {
         name: 'Diploma in Building Technology',
         minimumGrade: 'C-',
-        examiningBody: 'KNEC',
+        examiningBody: cdacc_exam_body,
         duration: `${diploma_duration} years`
       },
       {
         name: 'Diploma in Business Management',
         minimumGrade: 'C-',
-        examiningBody: 'KNEC',
+        examiningBody: cdacc_exam_body,
         duration: `${diploma_duration} years`
       },
       {
         name: 'Diploma in Supply Chain Management',
         minimumGrade: 'C-',
-        examiningBody: 'KNEC',
+        examiningBody: cdacc_exam_body,
         duration: `${diploma_duration} years`
       },
       {
@@ -57,31 +60,31 @@ export const coursesData: CourseCategory[] = [
       {
         name: 'Diploma in Electrical Engineering',
         minimumGrade: 'C-',
-        examiningBody: 'KNEC',
+        examiningBody: cdacc_exam_body,
         duration: `${diploma_duration} years`
       },
       {
         name: 'Diploma in Social Work and Community Development',
         minimumGrade: 'C-',
-        examiningBody: 'KNEC',
+        examiningBody: cdacc_exam_body,
         duration: `${diploma_duration} years`
       },
       {
         name: 'Diploma in Library Science',
         minimumGrade: 'C-',
-        examiningBody: 'KNEC',
+        examiningBody: cdacc_exam_body,
         duration: `${diploma_duration} years`
       },
       {
         name: 'Diploma in Fashion and Design',
         minimumGrade: 'C-',
-        examiningBody: 'KNEC',
+        examiningBody: cdacc_exam_body,
         duration: `${diploma_duration} years`
       },
       {
         name: 'Diploma in Secretarial Studies',
         minimumGrade: 'C-',
-        examiningBody: 'KNEC',
+        examiningBody: cdacc_exam_body,
         duration: `${diploma_duration} years`
       }
     ]
@@ -92,73 +95,73 @@ export const coursesData: CourseCategory[] = [
       {
         name: 'Plumbing',
         minimumGrade: 'D+',
-        examiningBody: 'KNEC',
+        examiningBody: cdacc_exam_body,
         duration: `${craft_duration} years`
       },
       {
         name: 'Masonry TEP',
         minimumGrade: 'D+',
-        examiningBody: 'KNEC',
+        examiningBody: cdacc_exam_body,
         duration: `${craft_duration} years`
       },
       {
         name: 'Information and Communications Technology',
         minimumGrade: 'D+',
-        examiningBody: 'KNEC',
+        examiningBody: cdacc_exam_body,
         duration: `${craft_duration} years`
       },
       {
         name: 'Civil Engineering',
         minimumGrade: 'D+',
-        examiningBody: 'KNEC',
+        examiningBody: cdacc_exam_body,
         duration: `${craft_duration} years`
       },
       {
         name: 'Building Technology',
         minimumGrade: 'D+',
-        examiningBody: 'KNEC',
+        examiningBody: cdacc_exam_body,
         duration: `${craft_duration} years`
       },
       {
         name: 'Information Technology',
         minimumGrade: 'D+',
-        examiningBody: 'KNEC',
+        examiningBody: cdacc_exam_body,
         duration: `${craft_duration} years`
       },
       {
         name: 'Business Management',
         minimumGrade: 'D+',
-        examiningBody: 'KNEC',
+        examiningBody: cdacc_exam_body,
         duration: `${craft_duration} years`
       },
       {
         name: 'Supply Chain Management',
         minimumGrade: 'D+',
-        examiningBody: 'KNEC',
+        examiningBody: cdacc_exam_body,
         duration: `${craft_duration} years`
       },
       {
         name: 'Secretarial Single and Group',
         minimumGrade: 'D+',
-        examiningBody: 'KNEC',
+        examiningBody: cdacc_exam_body,
         duration: `${craft_duration} years`
       },
       {
         name: 'Electrical Installation',
         minimumGrade: 'D+',
-        examiningBody: 'KNEC',
+        examiningBody: cdacc_exam_body,
         duration: `${craft_duration} years`
       },
       {
         name: 'Library and Information Science',
         minimumGrade: 'D+',
-        examiningBody: 'KNEC',
+        examiningBody: cdacc_exam_body,
         duration: `${craft_duration} years`
       },
       {
         name: 'Clothing and Textile',
         minimumGrade: 'D+',
-        examiningBody: 'KNEC',
+        examiningBody: cdacc_exam_body,
         duration: `${craft_duration} years`
       }
     ]
@@ -169,31 +172,31 @@ export const coursesData: CourseCategory[] = [
         {
             name: 'Plumbing',
             minimumGrade: 'D',
-            examiningBody: 'KNEC',
+            examiningBody: cdacc_exam_body,
             duration: `${artisan_duration} years`,
           },
           {
             name: 'Building Technology',
             minimumGrade: 'D',
-            examiningBody: 'KNEC',
+            examiningBody: cdacc_exam_body,
             duration: `${artisan_duration} years`,
           },
           {
             name: 'Masonry (TEP)',
             minimumGrade: 'D',
-            examiningBody: 'KNEC',
+            examiningBody: cdacc_exam_body,
             duration: `${artisan_duration} years`,
           },
           {
             name: 'Carpentry',
             minimumGrade: 'D',
-            examiningBody: 'KNEC',
+            examiningBody: cdacc_exam_body,
             duration: `${artisan_duration} years`,
           },
           {
             name: 'Motor Vehicle',
             minimumGrade: 'D',
-            examiningBody: 'KNEC',
+            examiningBody: cdacc_exam_body,
             duration: `${artisan_duration} years`,
           },
     ]
@@ -205,37 +208,37 @@ export const coursesData: CourseCategory[] = [
             name: 'Electrical Wiring',
             duration: '6 Months',
             minimumGrade: 'N/A',
-            examiningBody: 'Internal',
+            examiningBody: ssc_exam_body,
           },
           {
             name: 'Spray Painting',
             duration: '4 Months',
             minimumGrade: 'N/A',
-            examiningBody: 'Internal',
+            examiningBody: ssc_exam_body,
           },
           {
             name: 'Cabro Laying',
             duration: '4 Months',
             minimumGrade: 'N/A',
-            examiningBody: 'Internal',
+            examiningBody: ssc_exam_body,
           },
           {
             name: 'Dress Making',
             duration: '6 Months',
             minimumGrade: 'N/A',
-            examiningBody: 'Internal',
+            examiningBody: ssc_exam_body,
           },
           {
             name: 'Computer Packages',
             duration: '6 Months',
             minimumGrade: 'N/A',
-            examiningBody: 'Internal',
+            examiningBody: ssc_exam_body,
           },
           {
             name: 'Hairdressing and Beauty Therapy',
             duration: '4 Months',
             minimumGrade: 'N/A',
-            examiningBody: 'Internal',
+            examiningBody: ssc_exam_body,
           },
     ]
   }
